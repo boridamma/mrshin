992c0f4c8d2849b8e944c83f1d311f5eb8af7617
boridamma <70124505+boridamma@users.noreply.github.com>
2025-03-26 14:38:49 +0900

    Update snakeGame.js

--- snakeGame.js
+++ snakeGame.js
@@ -3,6 +3,9 @@ const ctx = canvas.getContext("2d");
 
 const box = 20;
 let snake, direction, food, game;
+const snakeHeadImg = new Image();
+snakeHeadImg.src = "mrshin.png"; // Load the image for the snake head
+
 startGame();
 
 function startGame() {
@@ -23,7 +26,7 @@ window.addEventListener("keydown", (event) => {
     if (event.key === "ArrowDown" && direction !== "UP") direction = "DOWN";
     if (event.key === "ArrowLeft" && direction !== "RIGHT") direction = "LEFT";
     if (event.key === "ArrowRight" && direction !== "LEFT") direction = "RIGHT";
-    
+
     // Restart the game when Enter is pressed
     if (event.key === "Enter") startGame();
 });
@@ -31,23 +34,28 @@ window.addEventListener("keydown", (event) => {
 function draw() {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
+
     ctx.fillStyle = "red";
     ctx.fillRect(food.x, food.y, box, box);
-    
+
     for (let i = 0; i < snake.length; i++) {
-        ctx.fillStyle = i === 0 ? "lime" : "green";
-        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+        if (i === 0) {
+            // Draw snake head with image
+            ctx.drawImage(snakeHeadImg, snake[i].x, snake[i].y, box, box);
+        } else {
+            ctx.fillStyle = "green";
+            ctx.fillRect(snake[i].x, snake[i].y, box, box);
+        }
     }
-    
+
     let newX = snake[0].x;
     let newY = snake[0].y;
-    
+
     if (direction === "UP") newY -= box;
     if (direction === "DOWN") newY += box;
     if (direction === "LEFT") newX -= box;
     if (direction === "RIGHT") newX += box;
-    
+
     if (newX === food.x && newY === food.y) {
         food = {
             x: Math.floor(Math.random() * 20) * box,
@@ -56,15 +64,15 @@ function draw() {
     } else {
         snake.pop();
     }
-    
+
     let newHead = { x: newX, y: newY };
-    
+
     if (newX < 0 || newX >= canvas.width || newY < 0 || newY >= canvas.height ||
         snake.some(segment => segment.x === newX && segment.y === newY)) {
         clearInterval(game);
         alert("Game Over! Press Enter to restart.");
         return;
     }
-    
+
     snake.unshift(newHead);
 }
